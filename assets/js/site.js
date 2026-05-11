7fd4a8eb6c9e710e795a7efc6a2b6fdbbe8429ab
VitaliiMR <vimo8@elev.techcollege.dk>
2026-05-11 09:10:58 +0200

    asd

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,2 +1,6 @@
 console.log('stand proud, you can code!');
 
+function changeImage() {
+    const mainImage = document.getElementById('mainImage');
+    const imgPick = document.querySelectorAll('#imgPick');
+}
